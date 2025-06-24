8cc9d25ef791fc8a83f91bf2ed01ddaca990e8d9
Christian Niklas <christian.niklas@outletcity.com>
2025-06-24 07:54:31 +0200

    add average

--- src/components/PokerButtons.vue
+++ src/components/PokerButtons.vue
@@ -8,7 +8,10 @@ const { myUuid } = defineProps<{
 	myUuid: string
 }>()
 
-const storyPointList = ['?', '☕️', '1', '2', '3', '5', '8', '13', '20', '40', '100']
+const votingFinished = computed(() => state.room?.voting_enabled === false)
+
+const STORY_POINT_LIST = ['?', '☕️', '1', '2', '3', '5', '8', '13', '20', '40', '100']
+
 const distribution = computed(() => {
 	const distribution = new Map<string, number>()
 	state.storyPoints.forEach(vote => {
@@ -16,20 +19,23 @@ const distribution = computed(() => {
 	})
 	return distribution
 })
+const average = computed(() => {
+	if (!state.storyPoints.size) return 0
 
-const vote = (storyPoints: string) => {
-	addVote(myUuid, storyPoints)
-}
+	const numbers = Array.from(state.storyPoints.values()).filter(val => !isNaN(+val))
+	const sum = numbers.reduce((acc, val) => acc + +val, 0)
+	return (sum / numbers.length).toFixed(1)
+})
 </script>
 
 <template>
+	<div v-if="votingFinished">Durchschnitt: {{ average }}</div>
+
 	<ul class="flex flex-wrap gap-4">
-		<li v-for="(storyPoints, i) in storyPointList" :key="i">
-			<button type="button" @click="vote(storyPoints)">
+		<li v-for="(storyPoints, i) in STORY_POINT_LIST" :key="i">
+			<button type="button" @click="addVote(myUuid, storyPoints)">
 				{{ storyPoints }}
-				<small v-if="state.room?.voting_enabled === false" aria-hidden="true">{{
-					distribution.get(storyPoints)
-				}}</small>
+				<small v-if="votingFinished" aria-hidden="true">{{ distribution.get(storyPoints) }}</small>
 			</button>
 		</li>
 	</ul>
